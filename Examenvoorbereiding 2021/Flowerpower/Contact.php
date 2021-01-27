8992c74b817f2dec7c59f7920e08ed7ff6c2f087
Lbuildr <54835112+Lbuildr@users.noreply.github.com>
2021-01-27 12:14:30 +0100

    Add files via upload

---
+++ Examenvoorbereiding 2021/Flowerpower/Contact.php
@@ -0,0 +1,8 @@
+<?php
+include_once("menu.html");
+
+print "<h2>Contact</h2>";
+print "Flowerpower<br>";
+print "JanFrietstraat 123
+1089 HJ Amsterdam";
+?>
---
+++ Examenvoorbereiding 2021/Flowerpower/index.php
@@ -0,0 +1,15 @@
+<?php
+include_once("menu.html");
+include_once("Database.php");
+
+echo ' 
+<!DOCTYPE html">
+<img src="../Media/winkel.jpg" />
+<img src="../Media/boeket.jpg" />
+</html> ';
+
+
+<!DOCTYPE HTML>
+
+
+?>
